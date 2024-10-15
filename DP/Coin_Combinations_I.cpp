04a2709679972038845009e1f128069068055bd8
onlyapon <ajmain.apon123@gmail.com>
2024-10-15 17:39:37 +0600

    Create Coin_Combinations_I.cpp

---
+++ DP/Coin_Combinations_I.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+#define ll long long
+#define all(a) a.begin(),a.end()
+int main(){
+	ll n,x;cin>>n>>x;
+	vector<ll> coin(n);
+	vector<ll> dp(x+1,0);
+	for(auto &i:coin)cin>>i;
+	dp[0]=1;
+	for (ll i = 1; i <= x; ++i)
+	{
+		for (ll j = 0; j < n ; ++j)
+		{
+			if(coin[j]<=i)
+				dp[i]=(dp[i]+dp[i-coin[j]])%MOD;
+		}
+	}
+	cout<<dp[x];
+	
+}	
